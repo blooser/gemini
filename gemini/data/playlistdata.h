cfc5a268eaf5b5a3adaa99b3a0c5cf55839401ad
blooser <Blooser@protonmail.com>
2020-04-17 00:32:00 +0200

    PlaylistData contains songs and duration
    Signed-off-by: blooser <Blooser@protonmail.com>

--- gemini/data/playlistdata.h
+++ gemini/data/playlistdata.h
@@ -12,10 +12,14 @@ struct Playlist {
     Q_GADGET
     Q_PROPERTY(int id MEMBER m_id)
     Q_PROPERTY(QString name MEMBER m_name)
+    Q_PROPERTY(int songs MEMBER m_songs)
+    Q_PROPERTY(int duration MEMBER m_duration)
 
 public:
     int m_id;
     QString m_name;
+    int m_songs;
+    int m_duration;
 
     friend inline bool operator==(const Playlist &playlist1, const Playlist &playlist2) {
         return (playlist1.m_id == playlist2.m_id);
@@ -26,12 +30,14 @@ public:
     };
 
     friend QDataStream &operator<<(QDataStream &out, const Playlist &playlist) {
-        return out << playlist.m_id << playlist.m_name;
+        return out << playlist.m_id << playlist.m_name
+                   << playlist.m_songs << playlist.m_duration;
     }
 
 
     friend QDataStream &operator>>(QDataStream &in, Playlist &playlist) {
-        return in >> playlist.m_id >> playlist.m_name;
+        return in >> playlist.m_id >> playlist.m_name
+                  >> playlist.m_songs >> playlist.m_duration;
     }
 };
 
